819ba2d1b1c924a9be6ea41e4705d117e1e14ca8
coldcisar <cesar.rubio.comp@gmail.com>
2023-07-07 18:05:09 -0400

    terminado login

--- sistema/views.py
+++ sistema/views.py
@@ -94,7 +94,7 @@ def paginaLogin(request):
             return render(request, 'index.html')
         except nuevoUsuario.DoesNotExist as e:
             messages.success(request,'Usuario no existe')
-    return render(request, 'inicio.html')
+    return render(request, 'login.html')
 
 def cerrar_sesion(request):
     template_name='inicio.html'
